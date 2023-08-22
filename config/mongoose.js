04eef885c57a7ea4e9cf069794c0ce4fdaae7f37
YugalRajVimal <sagaryugal472@gmail.com>
2023-08-22 23:29:56 +0530

    Insertion, Deletion, Fetching is Done using MongoDB,Mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,49 @@
+// const mongoose = require('mongoose');
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+// }
+
+// const db = mongoose.connection;
+
+// //error
+// db.on('error', function(err) { console.log(err.message); });
+
+// //up and running then print the message
+// db.once('open', function() {
+  
+//     console.log("Successfully connected to the database");
+
+// });
+
+
+
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contact-list-db');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+// mongoose.connect('mongodb://127.0.0.1:27017/contact-list-db');
+
+//acquire the connection(to check if it's successful)
+const db = mongoose.connection;
+
+//error
+db.on('error', function(err) { console.log(err.message); });
+
+//up and running then print the message
+db.once('open', function() {
+  
+    console.log("Successfully connected to the database");
+
+});
--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express=require('express');
 const path = require('path');
 const port=8000;
 
-// Error
-    //For Mongoose
-    // const db = require('./config/mongoose')
-    // const Contact = require('./models/contact')
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
 
 const app=express();
 
@@ -14,44 +12,28 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
-// // Middleware 1
+/*
 
-// app.use(function(req,res,next){
+    var contactList = [
+        {
+            name:"Arpan",
+            phone:"1111111111"
+        },
+        {
+            name:"Tony Stark",
+            phone:"2222222222"
+        },
+        {
+            name:"Abcd",
+            phone:"3333333333"
+        },
+        {
+            name:"Xyz",
+            phone:"4444444444"
+        }
+    ]
 
-//     req.myName =  'Sagar';
-
-//     // console.log("Middleware 1 Called");
-//     next();
-// });
-
-// //Middleware 2
-
-// app.use(function(req,res,next){
-
-//     console.log("From Middleware 2 : ",req.myName);
-
-//     // console.log("Middleware 2 Called");
-//     next();
-// });
-
-var contactList = [
-    {
-        name:"Arpan",
-        phone:"1111111111"
-    },
-    {
-        name:"Tony Stark",
-        phone:"2222222222"
-    },
-    {
-        name:"Abcd",
-        phone:"3333333333"
-    },
-    {
-        name:"Xyz",
-        phone:"4444444444"
-    }
-]
+*/
 
 app.get('/practice',function(request,response){
     return response.render('practice',{
@@ -59,82 +41,44 @@ app.get('/practice',function(request,response){
     });
 });
 
-app.get('/',/* async */ function(request,response){
+app.get('/',async function(request,response){
 
-    // console.log("From get route controller : ",request.myName);
-
-    // console.log(request)
-    // console.log(__dirname)
-    // response.send('<h1>Cool, it is running!</h1>');
-
-    return response.render('home',{
-        title: "Contact List",
-        contact_list: contactList
-    });
-
-    // Error
-        //Fetch the contact from Studio3T DB
-        // try {
-        //     let contacts = await  Contact.find({});
-        //     console.log("Contacts fetched from DB");
-        //     return response.render('home',{ 
-        //         title : "Contact List",
-        //         contact_list : contactList
-        //     });
-        // } catch (error) {
-        //     console.log('Error in fetching contacts from DB',error);
-        //     return;
-        // }
-   
+    try {
+        let contact = await Contact.find({});
+        return response.render('home',{
+            title: "Contact List",
+            contact_list: contact
+        });
+    } catch (error) {
+        console.log("Error in fetching contacts from DB");
+        return;
+    }
+    // return response.render('home',{
+    //     title: "Contact List",
+    //     contact_list: contactList
+    // });
     
 });
 
 
-app.post('/create_contact', /* async */ function(req,res){
-    // return res.redirect('practice');
+app.post('/create_contact',async function(req,res){
 
-    // Print 
-    // console.log(req.body);
-    // console.log(req.body.name);
-    // console.log(req.body.phone);
+    try {
+        let cont = await Contact.insertMany(
+            {
+                name:req.body.name,
+                phone:req.body.phone
+            }
+        );
+        return res.redirect('/');
+    } catch (error) {
+        console.log("Error in adding contacts in DB");
+        return;
+    }
 
-    //Insert
-    // contactList.push({
-    //     name: req.body.name,
-    //     phone: req.body.phone
-    // })
 
-    // Or
     // contactList.push(req.body);
-
-    contactList.push(req.body);
-    return res.redirect('/');
-
-    //Error
-        // try {
-        //     let contact = await Contact.create({
-        //         name:req.body.name,
-        //         phone:req.body.phone
-        //     })
-        //     console.log("Contact Created ",contact);
-        //     return res.redirect('back');
-        // } catch (error) {
-        //     console.log('Error in creating a contact',error);
-        //     return res.redirect('back');
-        // }
-
-    // function(err,newContact){
-    //     if(err){
-    //         console.log('Error in creating a contact');
-    //         return;
-    //     }
-    //         console.log('*********',newContact);
-    //         return res.redirect('back');
-    // }
-
-    
-
-    // return res.redirect('back');
+    // return res.redirect('/');
 
 });
 
@@ -146,55 +90,29 @@ app.listen(port,function(err){
 });
 
 // for deleting a contact
-app.get('/delete-contact', /* async */ function(req,res){
+app.get('/delete-contact',async function(req,res){
 
-    // Error
-        // try {
-        //     //get the id from ul by the query
-        //     //Find the contact in the DB using id and delete it 
-        //     let contact =await Contact.findByIdAndDelete(req.query.id);
-
-        //     console.log("Contact deleted");
-        //     return res.redirect('back');
-        // } catch (error) {
-        //     console.log('Error in deleting a contact from DB',error);
-        //     return res.redirect('back');
-        // }
-
-
-        console.log(req.query);
-        let phone = req.query.phone
-    
-        let contactindex = contactList.findIndex(contact => contact.phone == phone);
-    
-        if(contactindex != -1){
-            contactList.splice(contactindex, 1);
+        try {
+            let id = req.query.id;
+            await Contact.deleteOne({_id : id});
+            return res.redirect('/');
+        } catch (error) {
+            console.log("Error in deleting contacts from DB");
+            return;
         }
+
+
+
+        // console.log(req.query);
+        // let phone = req.query.phone
     
-        return res.redirect('back');
+        // let contactindex = contactList.findIndex(contact => contact.phone == phone);
+    
+        // if(contactindex != -1){
+        //     contactList.splice(contactindex, 1);
+        // }
+    
+        // return res.redirect('back');
 
-
-    // console.log(req.query);
-
-    // // get the query from URL
-    // let phone = req.query.phone;
-    // // let index=0;
-
-    // // for(let i of contactList){ 
-    // //     if(i.phone == phone){
-    // //         break;
-    // //     }
-    // //     index++;
-    // // }
-
-    // // OR
-
-    // let index = contactList.findIndex(contact => contact.phone == phone);
-
-    // if(index != -1){
-    //     contactList.splice(index,1);
-    // }
-
-    // return res.redirect('back');
 });
 
